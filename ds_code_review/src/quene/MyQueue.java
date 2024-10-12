a352b183d3dfd44a5dfa6b96d49505c5c587419b
JieCHERY <9594523wrj@gmail.com>
2024-10-13 00:50:14 +0800

    stack

---
+++ ds_code_review/src/quene/MyQueue.java
@@ -0,0 +1,25 @@
+package quene;
+
+import java.util.Collection;
+import java.util.NoSuchElementException;
+
+public class MyQueue {
+
+//    public interface Queue<E> extends Collection<E> {
+//        boolean add(E e);
+//        boolean offer(E e);
+//        E remove();
+//        E poll();
+//        E element();
+//        E peek();
+//    }
+    /**
+     * boolean offer(E e) 入队列
+     * E poll() 出队列
+     * peek() 获取队头元素但是不删除
+     * int size() 获取队列中有效元素个数
+     * boolean isEmpty() 检测队列是否为空
+     */
+
+
+}
